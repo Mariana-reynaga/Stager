73bf0b552aaa64f19c0560a4097d2f3ae8572c73
Mariana-reynaga <mariana.reynaga@davinci.edu.ar>
2025-02-18 11:51:19 -0300

    Overflow en la tarjeta de comision arreglado

--- resources/views/components/tarjeta-comision.blade.php
+++ resources/views/components/tarjeta-comision.blade.php
@@ -1,7 +1,9 @@
 <div>
     <div class="flex flex-col justify-evenly min-h-72 h-fit w-64 font-kanit text-negro p-3 border-2 border-rclaro rounded-md">
-        <p class=" font-bold text-lg mb-2">{{ $titulo }}</p>
-        <p class="my-1 text-balance">{{ $slot }}</p>
+        <p class="font-bold text-lg mb-2">{{ $titulo }}</p>
+        <div class="break-words overflow-hidden">
+            <p class="my-1">{{ $slot }}</p>
+        </div>
         <p class="mt-2"><span class="font-bold">Entrega:</span> {{ $fecha_entrega }}</p>
 
         <div class="flex justify-center mt-3">
