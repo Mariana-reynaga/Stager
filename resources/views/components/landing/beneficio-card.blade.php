b7587566a22e6de934f2c583f71479f55e669282
Mariana-reynaga <mariana.reynaga@davinci.edu.ar>
2025-03-07 13:00:57 -0300

    Estilos parciales a tarjeta de beneficio

--- resources/views/components/landing/beneficio-card.blade.php
+++ resources/views/components/landing/beneficio-card.blade.php
@@ -1,12 +1,14 @@
-<div class="flex justify-center my-5 md:my-0">
-    <div class="bg-rclaro p-3 rounded-lg flex flex-col items-center">
-        <div class="flex flex-col items-center">
-            <h3 class="text-blanco text-kanit font-bold">{{ $titulo }}</h3>
-            <p class="text-blanco text-center mt-3">{{ $slot }}</p>
-        </div>
+<div class="flex justify-center my-5 md:my-0 ">
+    <div class="w-2/3 md:w-full">
+        <div class="group md:h-60 p-3 flex flex-col items-center md:justify-around border-2 border-rclaro rounded-lg hover:border-none hover:bg-rclaro hover:shadow-xl hover:shadow-negro/30 transition duration-300 ease-in-out">
+            <div class="flex flex-col text-negro group-hover:text-blanco items-center font-kanit">
+                <h3 class="text-2xl font-bold">{{ $titulo }}</h3>
+                <p class="mt-3 text-center">{{ $slot }}</p>
+            </div>
 
-        <div class="bg-slate-400 w-1/2 min-h-20 mt-6">
-            meow
+            <div class="w-1/2 min-h-20 mt-6 flex justify-center">
+                <img src="/images/landing/{{$img}}" class="w-1/2">
+            </div>
         </div>
     </div>
 </div>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -30,29 +30,70 @@
     {{-- beneficios de stager --}}
     <div class="flex justify-center my-20" id="beneficios">
         <div class="w-4/5 flex flex-col items-center">
-        <h2 class="text-negro text-3xl font-kanit font-bold">Beneficios</h2>
+            <h2 class="text-negro text-3xl font-kanit font-bold text-center">Beneficios</h2>
+
+            {{-- <div
+                x-data="{
+                    currentSlide: 1,
+                    slides: [
+                        {id: 1, title: 'Conveniencia', tagline: 'Todos los detalles en un lugar.', img: `<img src='/images/landing/convinience.svg' class='w-10'>`},
+
+                        {id: 2, title: 'Simpleza', tagline: 'Diseño simple y ordenado.', img: `<img src='/images/landing/calendar.svg'  class='w-10'>` },
+
+                        {id: 3, title: 'Para artistas', tagline: 'Una plataforma creada especificamente para tus necesidades', img: `<img src='/images/landing/brush.svg'  class='w-10'>` },
+
+                        {id: 4, title: 'Gran Comunidad', tagline: 'Siempre estamos abiertos a sugerencias para un mejor servicio', img: `<img src='/images/landing/community.svg'  class='w-10'>` },
+                    ]
+                }"
+                >
+
+                <template x-for="slide in slides" :key="slide.id">
+                    <div class="flex justify-center my-5 md:my-0" x-show="currentSlide === slide.id">
+                        <div class="bg-rclaro p-3 rounded-lg flex flex-col items-center">
+                            <div class="flex flex-col items-center">
+                                <h3 class="text-blanco text-kanit font-bold" x-text="slide.title"></h3>
+                                <p class="text-blanco text-center mt-3" x-text="slide.tagline"></p>
+                            </div>
+
+                            <div class="w-1/2 min-h-20 mt-6">
+                                <span x-html="slide.img"></span>
+                            </div>
+                        </div>
+                    </div>
+                </template>
+
+                <div class="w-full flex items-center justify-center px-4">
+                    <template x-for="slide in slides" :key="slide.id">
+                      <button
+                        class="flex-1 w-4 h-2 mt-4 mx-2 mb-0 rounded-full overflow-hidden transition-colors duration-200 ease-out  hover:shadow-lg"
+                        :class="{
+                            'bg-rclaro hover:bg-rclaro': currentSlide === slide,
+                            'border-rclaro border-2 bg-white': currentSlide !== slide
+                        }"
+                        x-on:click="currentSlide = slide.id"
+                      ></button>
+                    </template>
+                </div>
+
+            </div> --}}
 
             {{-- beneficios --}}
             <div class="flex flex-col md:grid md:grid-cols-2 md:gap-x-12 md:gap-y-6 xl:flex xl:flex-row xl:justify-evenly xl:w-full mt-12">
 
-                <x-landing.beneficio-card>
-                    <x-slot name="titulo">Conveniencia</x-slot>
+                <x-landing.beneficio-card titulo="Conveniencia" img="convinience.svg">
                     Todos los detalles en un lugar
                 </x-landing.beneficio-card>
 
-                <x-landing.beneficio-card>
-                    <x-slot name="titulo">Simpleza</x-slot>
-                    Todos los detalles en un lugar
+                <x-landing.beneficio-card titulo="Simpleza" img="calendar.svg">
+                    Diseño simple y ordenado
                 </x-landing.beneficio-card>
 
-                <x-landing.beneficio-card>
-                    <x-slot name="titulo">Para artistas</x-slot>
-                    Todos los detalles en un lugar
+                <x-landing.beneficio-card titulo="Para artistas" img="brush.svg">
+                    Una plataforma creada especificamente para tus necesidades
                 </x-landing.beneficio-card>
 
-                <x-landing.beneficio-card>
-                    <x-slot name="titulo">Gran comunidad</x-slot>
-                    Todos los detalles en un lugar
+                <x-landing.beneficio-card titulo="Gran Comunidad" img="community.svg">
+                    Siempre estamos abiertos a sugerencias para un mejor servicio
                 </x-landing.beneficio-card>
 
             </div>
